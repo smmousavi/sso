99acc9c15e6525f3ad80ddfe7bb65186e4e96f8c
Your Name <your.email@example.com>
2025-12-19 12:35:35 +0330

    swagger route fixed

--- sso.api/Program.cs
+++ sso.api/Program.cs
@@ -9,9 +9,8 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
-
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "SSO API v1"));
 }
 
 app.UseAuthorization();
